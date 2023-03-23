0b2676faa4a99f0e45d39142d111a75215ead681
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-03-23 23:03:28 +0500

    Restart base

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
