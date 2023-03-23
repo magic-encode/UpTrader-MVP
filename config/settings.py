7c01f97a2bf70d831e307bbb02288f4856975fe3
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-03-23 22:34:59 +0500

    Restart base

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': "django.db.backends.postgresql",
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
         'PASSWORD': env.str('DB_PASSWORD'),
